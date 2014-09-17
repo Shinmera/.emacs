314793c1622af824e31ae6ffca170d2e43040283
Shinmera <shinmera@tymoon.eu>
2014-09-17 21:56:33 +0200

    Added autoinstall of most packages

--- shinmera-package.el
+++ shinmera-package.el
@@ -6,3 +6,21 @@
 (add-to-list 'package-archives       '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives       '("melpa"     . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
+
+;;;;;;;
+;; Autoinstall packages
+(require 'cl)
+(defvar *prelude-packages*
+  '(multiple-cursors expand-region auto-complete ecb
+    web-mode slime ac-slime paredit sublime-themes powerline
+    auctex markdown-mode rainbow-mode))
+
+(defun prelude-packages-installed-p ()
+  (loop for package in *prelude-packages*
+        always (package-installed-p package)))
+
+(unless (prelude-packages-installed-p)
+  (package-refresh-contents)
+  (dolist (package *prelude-packages*)
+    (when (not (package-installed-p package))
+      (package-install package))))
