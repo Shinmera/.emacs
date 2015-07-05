beb54159431e7e8c1f44dd4ebe2f60dc231f95fd
Shinmera <shinmera@tymoon.eu>
2015-07-05 17:54:04 +0200

    Made sure startup is only run after the init file is completely processed.

--- shinmera-startup.el
+++ shinmera-startup.el
@@ -1,9 +1,13 @@
 (provide 'shinmera-startup)
 
-(when window-system
-  (ecb-activate)
-  (slime))
+(defun startup-shinmera ()
+  (when window-system
+    (ecb-activate)
+    (slime))
 
-(unless (or (server-running-p)
-            (eq system-type 'windows-nt))
-  (server-start))
+  (unless (or (server-running-p)
+              (eq system-type 'windows-nt))
+    (server-start)))
+
+;; Make sure any user changes can be processed before this.
+(add-hook 'after-init-hook 'startup-shinmera)
