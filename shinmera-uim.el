674c5bcd9a05146fa2db48420b815c47249e41f7
Shinmera <shinmera@tymoon.eu>
2016-05-26 13:33:58 +0200

    Attempting to fix UIM stuff

--- shinmera-uim.el
+++ shinmera-uim.el
@@ -2,10 +2,15 @@
 
 ;;;;;;
 ;; Autoloading and setting.
-(autoload 'uim-mode "uim" "Uim-Mode" t)
+(require 'uim)
 
-(eval-after-load "uim"
-  '(setq
-    uim-default-im-engine "anthy"
-    uim-lang-code-alist   (cons '("Japanese" "Japanese" utf-8 "UTF-8")
-                                (delete (assoc "Japanese" uim-lang-code-alist) uim-lang-code-alist))))
+(setq
+ uim-default-im-engine "anthy-utf8"
+ uim-lang-code-alist (cons '("Japanese" "Japanese" utf-8 "UTF-8")
+                           (delete (assoc "Japanese" uim-lang-code-alist) uim-lang-code-alist))
+ uim-candidate-display-inline t
+ uim-default-im-prop '("action_anthy_utf8_hiragana"
+                       "action_anthy_hiragana"
+                       "action_google-cgiapi-jp_hiragana"
+                       "action_mozc_hiragana"))
+
