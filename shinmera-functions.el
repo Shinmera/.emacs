1957dd1efe4767897ed2f7c9deca541853e669d5
Shinmera <shinmera@tymoon.eu>
2017-07-05 09:20:11 +0200

    Should really be moved.

--- shinmera-functions.el
+++ shinmera-functions.el
@@ -15,20 +15,6 @@
         (kill-buffer buffer)
         (message "File '%s' successfully removed" filename)))))
 
-;; Auto-indent yank
-(dolist (command '(yank yank-pop))
-  (eval `(defadvice ,command (after indent-region activate)
-           (and (not current-prefix-arg)
-                (member major-mode '(emacs-lisp-mode lisp-mode
-                                                     clojure-mode    scheme-mode
-                                                     haskell-mode    ruby-mode
-                                                     rspec-mode      python-mode
-                                                     c-mode          c++-mode
-                                                     objc-mode       latex-mode
-                                                     plain-tex-mode))
-                (let ((mark-even-if-inactive transient-mark-mode))
-                  (indent-region (region-beginning) (region-end) nil))))))
-
 (defun sudo ()
   (interactive)
   (let ((position (point)))
--- shinmera-paste.el
+++ shinmera-paste.el
@@ -17,3 +17,17 @@
           xsel-output )))
     (setq interprogram-cut-function 'xsel-cut-function)
     (setq interprogram-paste-function 'xsel-paste-function)))
+
+;; Auto-indent yank
+(dolist (command '(yank yank-pop))
+  (eval `(defadvice ,command (after indent-region activate)
+           (and (not current-prefix-arg)
+                (member major-mode '(emacs-lisp-mode lisp-mode
+                                     clojure-mode    scheme-mode
+                                     haskell-mode    ruby-mode
+                                     rspec-mode      python-mode
+                                     c-mode          c++-mode
+                                     objc-mode       latex-mode
+                                     plain-tex-mode))
+                (let ((mark-even-if-inactive transient-mark-mode))
+                  (indent-region (region-beginning) (region-end) nil))))))
