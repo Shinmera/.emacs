21b5a1194f14fc49a7c23c5ec64142c5f97ffa76
Shinmera <shinmera@tymoon.eu>
2021-09-22 18:24:46 +0200

    add melpa-stable

--- shinmera-package.el
+++ shinmera-package.el
@@ -3,6 +3,7 @@
 (require 'package)
 ;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa"     . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 (package-initialize)
 
 ;;;;;;;
