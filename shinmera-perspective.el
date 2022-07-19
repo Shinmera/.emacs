8bf91af586c22df7a24c52af39e1843f347140e1
Shinmera <shinmera@tymoon.eu>
2022-07-19 13:26:52 +0200

    Minor

--- shinmera-perspective.el
+++ shinmera-perspective.el
@@ -3,6 +3,8 @@
 
 (require 'perspective)
 
+(global-set-key (kbd "C-x C-b") 'persp-list-buffers)
+(customize-set-variable 'persp-mode-prefix-key (kbd "C-c M-p"))
 (persp-mode)
 
 (provide 'shinmera-perspective)
