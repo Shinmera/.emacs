5c4c8018f39d24b58b59c6ce9edaa2e27dbca815
Shinmera <shinmera@tymoon.eu>
2024-01-07 19:34:25 +0100

    Remove rtags since it's kinda busted

--- shinmera-c.el
+++ shinmera-c.el
@@ -2,15 +2,12 @@
 (require 'shinmera-keys)
 
 (when (featurep 'shinmera-package)
-  (ensure-installed 'rtags 'rtags-xref 'irony 'company-irony 'company-c-headers
+  (ensure-installed 'irony 'company-irony 'company-c-headers
                     'cmake-ide 'cmake-mode 'realgud-lldb))
 
-(require 'rtags)
-(require 'rtags-xref)
 (add-hook 'c-mode-hook 'irony-mode)
 (add-hook 'c++-mode-hook 'irony-mode)
 (add-hook 'c-mode-hook 'flycheck-mode)
-(add-hook 'c-mode-hook 'rtags-xref-enable)
 (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
 
 (when (featurep 'shinmera-company)
