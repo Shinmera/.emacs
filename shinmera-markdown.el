ff94559e02d01e7259140828c161fa5afd662574
Shinmera <shinmera@tymoon.eu>
2024-05-14 18:30:47 +0200

    Start olivetti on text documents

--- shinmera-markdown.el
+++ shinmera-markdown.el
@@ -4,6 +4,7 @@
   :commands (markdown-mode)
   :mode
   ("\\.md\\'" . markdown-mode)
-  ("\\.markdown\\'" . markdown-mode))
+  ("\\.markdown\\'" . markdown-mode)
+  :hook (markless-mode . olivetti-mode))
 
 (provide 'shinmera-markdown)
--- shinmera-markless.el
+++ shinmera-markless.el
@@ -4,6 +4,7 @@
   :commands (markless-mode)
   :mode
   ("\\.mess\\'" . markless-mode)
-  ("\\.markless\\'" . markless-mode))
+  ("\\.markless\\'" . markless-mode)
+  :hook (markless-mode . olivetti-mode))
 
 (provide 'shinmera-markless)
