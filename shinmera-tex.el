c50776733fb6d53e95c0ffbb2fbe2aa614e932fb
Yukari Hafner <shinmera@tymoon.eu>
2025-01-17 10:32:50 +0100

    Fix TeX

--- shinmera-tex.el
+++ shinmera-tex.el
@@ -5,10 +5,8 @@
   :mode ("\\.tex\\'" . auctex-mode)
   :hook
   (TeX-mode . (lambda () (TeX-fold-mode 1)))
-  (TeX-mode . LaTeX-math-mode)
   :custom
   (TeX-engine 'luatex)
-  (LaTeX-math-abbrev-prefix "C-a")
   (LaTeX-babel-hyphen nil)
   (TeX-master nil)
   (TeX-PDF-mode t)
@@ -18,20 +16,7 @@
   (TeX-source-correlate-mode t)
   (TeX-source-correlate-start-server t)
   (TeX-view-program-list '(("Okular" "okular --unique %o#src:%n%b")))
-  (TeX-view-program-selection '((output-pdf "Okular")))
-  :config
-  (add-to-list 'TeX-command-list
-               '("Glossary" "makeglossaries %s" TeX-run-command nil
-                 (latex-mode)
-                 :help "Run makeglossaries script, which will choose xindy or makeindex") t)
-  (add-to-list 'TeX-command-list
-               '("GLaTeX" "%`%l%(mode)%' %t && makeglossaries %s && %`%l%(mode)%' %t" TeX-run-TeX nil
-                 (latex-mode)
-                 :help "Run LaTeX with Glossary processing"))
-  (add-to-list 'TeX-command-list
-               '("BLaTeX" "%`%l%(mode)%' %t && biber %s && %`%l%(mode)%' %t" TeX-run-TeX nil
-                 (latex-mode)
-                 :help "Run LaTeX with BibLaTeX processing")))
+  (TeX-view-program-selection '((output-pdf "Okular"))))
 
 (use-package company-auctex
   :demand t
